8812923fe254c0eb9583f696b8bbf5c59e8b020e
Danny Davidson <contact@dannydavidson.com>
2016-04-18 15:20:59 -0500

    fix(build): include necessary settings for gcloud and install kubectl

--- bin/deploy-staging.sh
+++ bin/deploy-staging.sh
@@ -1,7 +1,9 @@
 echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
+gcloud components install kubectl
 gcloud --quiet components update
-gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
+gcloud config set compute/zone us-central1-a
 gcloud config set project $GCLOUD_PROJECT
+gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
 gcloud container clusters get-credentials staging
 gcloud docker build .
 gcloud docker tag ledger-graph gcr.io/ledger-graph/ledger-graph
