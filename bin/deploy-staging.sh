abb048a0063747c9a7f6c0e7fa2cffd76090b526
Danny Davidson <contact@dannydavidson.com>
2016-04-18 18:06:23 -0500

    fix(build): move component update and quiet build output

--- bin/deploy-staging.sh
+++ bin/deploy-staging.sh
@@ -1,4 +1,8 @@
 echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
+
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update kubectl
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
+
 sudo /opt/google-cloud-sdk/bin/gcloud  auth activate-service-account --key-file ${HOME}/client-secret.json
 
 sudo /opt/google-cloud-sdk/bin/gcloud  config set container/cluster staging
@@ -7,11 +11,8 @@ sudo /opt/google-cloud-sdk/bin/gcloud  container clusters get-credentials stagin
 sudo /opt/google-cloud-sdk/bin/gcloud  config set compute/zone us-central1-a
 sudo /opt/google-cloud-sdk/bin/gcloud  config set project $GCLOUD_PROJECT
 
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update kubectl
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
-
-docker build -t ledger-graph .
-docker tag ledger-graph gcr.io/ledger-graph/ledger-graph
+sudo docker build -q -t ledger-graph .
+sudo docker tag ledger-graph gcr.io/ledger-graph/ledger-graph
 sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/ledger-graph/ledger-graph
 
 sudo kubectl delete -f ledger-graph-rc.json
