cb66a087b0b1bc65fe453b5a98978324d66ddd9d
Danny Davidson <contact@dannydavidson.com>
2016-04-18 15:26:11 -0500

    fix(build): reorder commands

--- bin/deploy-staging.sh
+++ bin/deploy-staging.sh
@@ -1,12 +1,17 @@
 echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
-gcloud components install kubectl
-gcloud --quiet components update
+gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
+
 gcloud config set compute/zone us-central1-a
 gcloud config set project $GCLOUD_PROJECT
-gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
+
 gcloud container clusters get-credentials staging
+
+gcloud --quiet components update
+gcloud --quiet components install kubectl
+
 gcloud docker build .
 gcloud docker tag ledger-graph gcr.io/ledger-graph/ledger-graph
 gcloud docker push gcr.io/ledger-graph/ledger-graph
+
 kubectl delete -f ledger-graph-rc.json
 kubectl create -f ledger-graph-rc.json
