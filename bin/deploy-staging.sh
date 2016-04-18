1b5cf8e610720c7909e2dbf852e3bda78999be9f
Danny Davidson <contact@dannydavidson.com>
2016-04-18 18:54:39 -0500

    fix(build): version builds so we can rolling update

--- bin/deploy-staging.sh
+++ bin/deploy-staging.sh
@@ -1,3 +1,5 @@
+VERSION="$(git rev-parse HEAD)"
+
 echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
 
 sudo /opt/google-cloud-sdk/bin/gcloud config set compute/zone us-central1-a
@@ -11,8 +13,8 @@ sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file $
 sudo /opt/google-cloud-sdk/bin/gcloud config set container/cluster staging
 sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials staging
 
-sudo docker build -q -t ledger-graph .
-sudo docker tag ledger-graph gcr.io/ledger-graph/ledger-graph
-sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/ledger-graph/ledger-graph
+sudo docker build -q -t ledger-graph:${VERSION} .
+sudo docker tag ledger-graph:${VERSION} gcr.io/ledger-graph/ledger-graph:${VERSION}
+sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/ledger-graph/ledger-graph:${VERSION}
 
-sudo /opt/google-cloud-sdk/bin/kubectl rolling-update ledger-graph -f ledger-graph-rc.json
+sudo /opt/google-cloud-sdk/bin/kubectl rolling-update ledger-graph ledger-graph:${VERSION} --image=gcr.io/ledger-graph/ledger-graph:${VERSION}
