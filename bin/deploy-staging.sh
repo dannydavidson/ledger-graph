153e73ac527f63713a65e7aab70991aaaac58cec
Danny Davidson <contact@dannydavidson.com>
2016-04-18 17:59:44 -0500

    fix(build): try with sudo

--- bin/deploy-staging.sh
+++ bin/deploy-staging.sh
@@ -1,19 +1,18 @@
 echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
-gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
+sudo /opt/google-cloud-sdk/bin/gcloud  auth activate-service-account --key-file ${HOME}/client-secret.json
 
-gcloud config set container/cluster staging
-gcloud container clusters get-credentials staging
+sudo /opt/google-cloud-sdk/bin/gcloud  config set container/cluster staging
+sudo /opt/google-cloud-sdk/bin/gcloud  container clusters get-credentials staging
 
-gcloud config set compute/zone us-central1-a
-gcloud config set project $GCLOUD_PROJECT
-gcloud config set
+sudo /opt/google-cloud-sdk/bin/gcloud  config set compute/zone us-central1-a
+sudo /opt/google-cloud-sdk/bin/gcloud  config set project $GCLOUD_PROJECT
 
-gcloud --quiet components update
-gcloud --quiet components update kubectl
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update kubectl
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
 
 docker build -t ledger-graph .
 docker tag ledger-graph gcr.io/ledger-graph/ledger-graph
-gcloud docker push gcr.io/ledger-graph/ledger-graph
+sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/ledger-graph/ledger-graph
 
-kubectl delete -f ledger-graph-rc.json
-kubectl create -f ledger-graph-rc.json
+sudo kubectl delete -f ledger-graph-rc.json
+sudo kubectl create -f ledger-graph-rc.json
