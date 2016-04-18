c646db511faa1855b25d0ec8a73c894cf34d582d
Danny Davidson <contact@dannydavidson.com>
2016-04-18 16:20:12 -0500

    fix(build): reorder and add container cluster to config

--- bin/deploy-staging.sh
+++ bin/deploy-staging.sh
@@ -1,13 +1,15 @@
 echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
 gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
 
-gcloud config set compute/zone us-central1-a
-gcloud config set project $GCLOUD_PROJECT
-
+gcloud config set container/cluster staging
 gcloud container clusters get-credentials staging
 
+gcloud config set compute/zone us-central1-a
+gcloud config set project $GCLOUD_PROJECT
+gcloud config set
+
 gcloud --quiet components update
-gcloud --quiet components install kubectl
+gcloud --quiet components update kubectl
 
 docker build -t ledger-graph .
 docker tag ledger-graph gcr.io/ledger-graph/ledger-graph
