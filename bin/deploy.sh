3238d62969cbac1bd2ac2fa0f98a1d64a2f4107f
Danny Davidson <contact@dannydavidson.com>
2016-04-30 22:49:45 -0500

    chore(cleanup): remove echo

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,7 +13,6 @@ sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file $
 sudo /opt/google-cloud-sdk/bin/gcloud config set container/cluster api-dannydavidson-com
 sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials api-dannydavidson-com
 
-echo "Building and pushing ledger-graph:${VERSION}"
 sudo docker build -q -t ledger-graph:${VERSION} .
 sudo docker tag ledger-graph:${VERSION} gcr.io/api-dannydavidson-com/ledger-graph:${VERSION}
 sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/api-dannydavidson-com/ledger-graph:${VERSION}
