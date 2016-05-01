fa9dcd1d73380dbbac1f760e50f733787a635869
Danny Davidson <contact@dannydavidson.com>
2016-05-01 12:04:14 -0500

    fix(db-creds): update db creds to set secret for use by DB and app

--- bin/set-db-credentials.sh
+++ bin/set-db-credentials.sh
@@ -1,5 +1,9 @@
-user=$(echo $1 | base64)
-password=$(echo $2 | base64)
+user=$(echo 'neo4j' | base64)
+password=$(echo $1 | base64)
+auth=$(echo "${user}/${password}" | base64)
 
 sed "s/{{user}}/${user}/g;s/{{password}}/${password}/g" db-credentials.yml > db-credentials.set.yml
+sed "s/{{auth}}/${auth}/g" neo4j-admin-credentials.yml > neo4j-admin-credentials.set.yml
+
+kubectl apply -f neo4j-admin-credentials.set.yml
 kubectl apply -f db-credentials.set.yml
