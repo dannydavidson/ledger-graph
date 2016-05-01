25b8525b4a3cbefc1b1def99cd5ba5a2d1e70662
Danny Davidson <contact@dannydavidson.com>
2016-05-01 00:49:20 -0500

    debug(health): set it back

--- handlers/health.js
+++ handlers/health.js
@@ -2,15 +2,14 @@
 
 exports.isOk = function(db, logger) {
   return (req, res) => {
-    res.status(200).send('HEALTHY');
-    // db.read(0, (err) => {
-    //   if (err && err.statusCode !== 404) {
-    //     logger.emerg('Neo4j connection failed', err);
-    //     return res.status(500).send('UNHEALTHY');
-    //   }
-    //
-    //   logger.notice('Health Checked Successfully');
-    //   res.status(200).send('HEALTHY');
-    // });
+    db.read(0, (err) => {
+      if (err && err.statusCode !== 404) {
+        logger.emerg('Neo4j connection failed', err);
+        return res.status(500).send('UNHEALTHY');
+      }
+
+      logger.notice('Health Checked Successfully');
+      res.status(200).send('HEALTHY');
+    });
   };
 };
