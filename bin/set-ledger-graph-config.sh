32a9d15fc6698ceba2d616b10c984618e4599753
Danny Davidson <contact@dannydavidson.com>
2016-05-02 23:20:01 -0500

    debug(routes): debug var the right way

--- bin/set-ledger-graph-config.sh
+++ bin/set-ledger-graph-config.sh
@@ -1,5 +1,6 @@
 MOUNT_PATH=$(echo '/ledger-graph' | base64)
+DEBUG=$(echo 'express:router' | base64)
 
-sed "s/{{MOUNT_PATH}}/${MOUNT_PATH}/g" ledger-graph-config.yml > ledger-graph-config.set.yml
+sed "s/{{MOUNT_PATH}}/${MOUNT_PATH}/g;s/{{DEBUG}}/${DEBUG}/g" ledger-graph-config.yml > ledger-graph-config.set.yml
 
 kubectl apply -f ledger-graph-config.set.yml
