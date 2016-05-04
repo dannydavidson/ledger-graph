4e2ccf8b73549ab9b84c4d20c4ca8b8d69519373
Danny Davidson <contact@dannydavidson.com>
2016-05-03 22:12:17 -0500

    fix(secrets): fix up secrets

--- bin/set-db-credentials.sh
+++ bin/set-db-credentials.sh
@@ -1,9 +1,6 @@
-user=$(echo 'neo4j' | base64)
-password=$(echo $1 | base64)
-auth=$(echo "neo4j/$1" | base64)
+user=$(echo -n 'neo4j' | base64)
+password=$(echo -n  $1 | base64)
 
 sed "s/{{user}}/${user}/g;s/{{password}}/${password}/g" db-credentials.yml > db-credentials.set.yml
-sed "s/{{auth}}/${auth}/g" neo4j-admin-credentials.yml > neo4j-admin-credentials.set.yml
 
-kubectl apply -f neo4j-admin-credentials.set.yml
 kubectl apply -f db-credentials.set.yml
--- bin/set-ledger-graph-config.sh
+++ bin/set-ledger-graph-config.sh
@@ -1,6 +1,5 @@
-MOUNT_PATH=$(echo '/ledger-graph' | base64 | tr -d '\n')
-DEBUG=$(echo 'express:router' | base64 | tr -d '\n')
+MOUNT_PATH=$(echo -n '/ledger-graph' | base64)
 
-sed "s/{{MOUNT_PATH}}/${MOUNT_PATH}/g;s/{{DEBUG}}/${DEBUG}/g" ledger-graph-config.yml > ledger-graph-config.set.yml
+sed "s/{{MOUNT_PATH}}/${MOUNT_PATH}/g" ledger-graph-config.yml > ledger-graph-config.set.yml
 
 kubectl apply -f ledger-graph-config.set.yml
